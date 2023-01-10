f513be024ea89db872672f111bbf577dfd8dea60
yves51 <110249689+yves51@users.noreply.github.com>
2023-01-10 19:34:13 +0000

    exercice

---
+++ script.js
@@ -0,0 +1,24 @@
+/////////////////////////////////////////exercice 1/////////////////////////////////////////////
+const fruits = ["Banana", "Apples", "Oranges", "Blueberries"];
+let suppression  = fruits.splice(0,1);
+let trie = fruits.sort();
+let ajouterFin =fruits.push("Kiwi");
+let supprimer =  fruits.splice(0, 1)
+let tableauInverse = fruits.reverse();
+
+console.log(fruits);
+console.log(suppression);/**['Apples', 'Oranges', 'Blueberries'] */
+console.log(trie);/**['Apples', 'Blueberries', 'Oranges'] */
+console.log(ajouterFin);/**['Apples', 'Blueberries', 'Oranges', 'Kiwi'] */
+console.log(supprimer);/**['Blueberries', 'Oranges', 'Kiwi'] */
+console.log(tableauInverse);/***['Kiwi', 'Oranges', 'Blueberries'] */
+
+/////////////////////////////////////////exercice 2/////////////////////////////////////////////
+
+const moreFruits = ["Banana",
+                    ["Apples", 
+                      ["Oranges"], "Blueberries"]
+                    ];
+console.log(moreFruits[1][1][0]);/**Oranges */
+
+//debugger;
